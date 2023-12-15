c56d9041d16783f1bf210b6f1cf1e8ee4133119a
Drew Underwood <drew@uwood.net>
2023-12-15 10:25:34 -0500

    Concept is done, colors are changing and can be saved

--- src/main/kotlin/com/drewzillawood/customprogressbar/settings/CustomProgressBarConfigurable.kt
+++ src/main/kotlin/com/drewzillawood/customprogressbar/settings/CustomProgressBarConfigurable.kt
@@ -3,34 +3,66 @@ package com.drewzillawood.customprogressbar.settings
 import com.intellij.openapi.options.ConfigurationException
 import com.intellij.openapi.options.SearchableConfigurable
 import com.intellij.openapi.ui.DialogPanel
+import com.intellij.ui.ColorPanel
 import com.intellij.ui.dsl.builder.bindSelected
 import com.intellij.ui.dsl.builder.panel
+import com.intellij.util.EventDispatcher
+import java.awt.Color
+import java.util.*
 import javax.swing.JCheckBox
 import javax.swing.JComponent
+import kotlin.reflect.KFunction1
 
 class CustomProgressBarConfigurable : SearchableConfigurable {
-    private lateinit var panel: DialogPanel
 
-    private var isCustomProgressBarEnabled = true
+    private val myDispatcher = EventDispatcher.create(EventListener::class.java)
+
+    private lateinit var panel: DialogPanel
+    private lateinit var enabledCustomProgressBar : JCheckBox
+    private lateinit var myPrimaryColorChooser : ColorPanel
+    private lateinit var mySecondaryColorChooser : ColorPanel
+
+    private var settings = CustomProgressBarSettings.instance;
 
     override fun createComponent(): JComponent {
         panel = panel {
-            lateinit var myFirstCheckBox: JCheckBox
             row {
-                myFirstCheckBox = checkBox("Hello world: ")
-                    .bindSelected(::isCustomProgressBarEnabled)
+                enabledCustomProgressBar = checkBox("Enable Custom Progress Bar:")
+                    .bindSelected(settings::isCustomProgressBarEnabled)
                     .component
+            }
+            row("Primary Color:") {
+                myPrimaryColorChooser = ColorPanel()
+                cell(myPrimaryColorChooser)
+                myPrimaryColorChooser.bindColor(
+                    settings::myPrimaryColor,
+                    settings::myPrimaryColor::set
+                )
+            }
+            row("Secondary Color:") {
+                mySecondaryColorChooser = ColorPanel()
+                cell(mySecondaryColorChooser)
+                mySecondaryColorChooser.bindColor(
+                    settings::mySecondaryColor,
+                    settings::mySecondaryColor::set
+                )
             }
         }
         return panel;
     }
 
     override fun isModified(): Boolean {
-        return panel.isModified()
+        return myPrimaryColorChooser.selectedColor != settings.myPrimaryColor
+                || mySecondaryColorChooser.selectedColor != settings.mySecondaryColor
+                || panel.isModified()
     }
 
     @Throws(ConfigurationException::class)
     override fun apply() {
+        val settings = CustomProgressBarSettings.instance
+        settings.isCustomProgressBarEnabled = enabledCustomProgressBar.isCursorSet
+        settings.myPrimaryColor = myPrimaryColorChooser.selectedColor!!
+        settings.mySecondaryColor = mySecondaryColorChooser.selectedColor!!
         panel.apply()
     }
 
@@ -41,4 +73,9 @@ class CustomProgressBarConfigurable : SearchableConfigurable {
     override fun getId(): String {
         return "preferences.custom.progress.bar"
     }
+}
+
+fun ColorPanel.bindColor(getter: () -> Color?, setter: KFunction1<Color, Unit>) {
+    this.selectedColor = getter()
+    selectedColor?.let { setter(it) }
 }
