01d9a2af89e659f67b69138709c7fbf6035bf840
Drew Underwood <drew@uwood.net>
2024-01-21 19:21:29 -0500

    Group/Version adjustment

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     id("org.jetbrains.intellij") version "1.13.3"
 }
 
-group = "com.CustomProgressBar"
-version = "1.0-SNAPSHOT"
+group = "com.drewzillawood.CustomProgressBar"
+version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
