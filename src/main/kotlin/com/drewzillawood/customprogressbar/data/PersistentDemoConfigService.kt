9953a5e029646c42155871d985ec9b88fe1d6b22
Drew Underwood <drew.underwood@jpmchase.com>
2024-07-14 13:06:40 -0400

    Remove "Demo" specific object

--- src/main/kotlin/com/drewzillawood/customprogressbar/data/PersistentDemoConfigService.kt
+++ src/main/kotlin/com/drewzillawood/customprogressbar/data/PersistentDemoConfigService.kt
@@ -1,8 +1,8 @@
 package com.drewzillawood.customprogressbar.data
 
-import com.drewzillawood.customprogressbar.data.model.PersistentDemoConfigs
+import com.drewzillawood.customprogressbar.data.model.PersistentConfigs
 
 interface PersistentDemoConfigService {
-  fun save(configs: PersistentDemoConfigs)
-  fun read(): PersistentDemoConfigs
+  fun save(configs: PersistentConfigs)
+  fun read(): PersistentConfigs
 }
