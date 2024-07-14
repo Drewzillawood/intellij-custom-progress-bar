d2af046ca80f994c733278e01547da728557a3d7
Drew Underwood <drew.underwood@jpmchase.com>
2024-07-14 17:32:41 -0400

    At least it's not Reflection :D

--- src/main/kotlin/com/drewzillawood/customprogressbar/data/model/PersistentConfigs.kt
+++ src/main/kotlin/com/drewzillawood/customprogressbar/data/model/PersistentConfigs.kt
@@ -1,32 +1,16 @@
 package com.drewzillawood.customprogressbar.data.model
 import com.intellij.openapi.components.BaseState
-import com.intellij.ui.JBColor
-import java.awt.Color
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
-private const val CYCLE_TIME_DEFAULT = 800
-private const val REPAINT_INTERVAL_DEFAULT = 50
-
-private val DEFAULT_PRIMARY_BLUE = Color(64, 115, 213)
-private val DEFAULT_SECONDARY_BLUE = Color(100, 148, 243)
-
-data class PersistentConfigs(
-  var myIndeterminatePrimaryColor: Int = DEFAULT_PRIMARY_BLUE.rgb,
-  var myIndeterminateSecondaryColor: Int = DEFAULT_SECONDARY_BLUE.rgb,
-  var myDeterminatePrimaryColor: Int = DEFAULT_PRIMARY_BLUE.rgb,
-  var myDeterminateSecondaryColor: Int = JBColor.GRAY.rgb,
-  var isCustomProgressBarEnabled: Boolean = true,
-  var isAdvancedOptionsEnabled: Boolean = false,
-  var cycleTime: Int = CYCLE_TIME_DEFAULT,
-  var repaintInterval: Int = REPAINT_INTERVAL_DEFAULT
-): BaseState() {
-  constructor(persistentConfigs: PersistentConfigs) : this(
-    persistentConfigs.myIndeterminatePrimaryColor,
-    persistentConfigs.myIndeterminateSecondaryColor,
-    persistentConfigs.myDeterminatePrimaryColor,
-    persistentConfigs.myDeterminateSecondaryColor,
-    persistentConfigs.isCustomProgressBarEnabled,
-    persistentConfigs.isAdvancedOptionsEnabled,
-    persistentConfigs.cycleTime,
-    persistentConfigs.repaintInterval
-  )
+@Serializable
+data class PersistentConfigs(@Transient var map: MutableMap<String, Any> = defaultPersistentConfigsMap()): BaseState() {
+  var myIndeterminatePrimaryColor: Int by map
+  var myIndeterminateSecondaryColor: Int by map
+  var myDeterminatePrimaryColor: Int by map
+  var myDeterminateSecondaryColor: Int by map
+  var isCustomProgressBarEnabled: Boolean by map
+  var isAdvancedOptionsEnabled: Boolean by map
+  var cycleTime: Int by map
+  var repaintInterval: Int by map
 }
