c0d65d101fa321c0a606002516418736f030f2a5
Drew Underwood <drew.underwood@jpmchase.com>
2024-07-17 18:19:01 -0400

    Renaming and more cleanup

--- src/main/kotlin/com/drewzillawood/customprogressbar/data/PersistentConfigsComponent.kt
+++ src/main/kotlin/com/drewzillawood/customprogressbar/data/PersistentConfigsComponent.kt
@@ -1,11 +1,9 @@
 package com.drewzillawood.customprogressbar.data
 
 import com.drewzillawood.customprogressbar.data.model.PersistentConfigs
-import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.SimplePersistentStateComponent
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
 
-@Service
-@State(name = "PersistentDemoConfigs", storages = [Storage("PersistentDemoConfigs.xml")])
-class PersistentDemoConfigServiceImpl : SimplePersistentStateComponent<PersistentConfigs>(PersistentConfigs())
+@State(name = "PersistentConfigs", storages = [Storage("PersistentConfigs.xml")])
+class PersistentConfigsComponent : SimplePersistentStateComponent<PersistentConfigs>(PersistentConfigs())
--- src/main/kotlin/com/drewzillawood/customprogressbar/data/PersistentDemoConfigsComponent.kt
+++ src/main/kotlin/com/drewzillawood/customprogressbar/data/PersistentDemoConfigsComponent.kt
@@ -1,11 +1,9 @@
 package com.drewzillawood.customprogressbar.data
 
 import com.drewzillawood.customprogressbar.data.model.PersistentConfigs
-import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.SimplePersistentStateComponent
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
 
-@Service
-@State(name = "PersistentConfigs", storages = [Storage("PersistentConfigs.xml")])
-class PersistentConfigServiceImpl : SimplePersistentStateComponent<PersistentConfigs>(PersistentConfigs())
+@State(name = "PersistentDemoConfigs", storages = [Storage("PersistentDemoConfigs.xml")])
+class PersistentDemoConfigsComponent : SimplePersistentStateComponent<PersistentConfigs>(PersistentConfigs())
