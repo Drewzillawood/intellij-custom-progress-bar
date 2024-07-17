fe2b4975d7c5038ec3e1d97e5e8d756530d0cce8
Drew Underwood <drew.underwood@jpmchase.com>
2024-07-17 18:08:39 -0400

    Additional cleanup

--- src/main/kotlin/com/drewzillawood/customprogressbar/data/model/PersistentConfigs.kt
+++ src/main/kotlin/com/drewzillawood/customprogressbar/data/model/PersistentConfigs.kt
@@ -12,7 +12,7 @@ private val DEFAULT_PRIMARY_BLUE = Color(64, 115, 213)
 private val DEFAULT_SECONDARY_BLUE = Color(100, 148, 243)
 
 @Serializable
-class PersistentConfigs() : BaseState() {
+class PersistentConfigs : BaseState() {
   var myIndeterminatePrimaryColor: Int by property(DEFAULT_PRIMARY_BLUE.rgb)
   var myIndeterminateSecondaryColor: Int by property(DEFAULT_SECONDARY_BLUE.rgb)
   var myDeterminatePrimaryColor: Int by property(DEFAULT_PRIMARY_BLUE.rgb)
@@ -20,14 +20,4 @@ class PersistentConfigs() : BaseState() {
   var isAdvancedOptionsEnabled: Boolean by property(false)
   var cycleTime: Int by property(CYCLE_TIME_DEFAULT)
   var repaintInterval: Int by property(REPAINT_INTERVAL_DEFAULT)
-
-  constructor(persistentConfigs: PersistentConfigs) : this() {
-    myIndeterminatePrimaryColor = persistentConfigs.myIndeterminatePrimaryColor
-    myIndeterminateSecondaryColor = persistentConfigs.myIndeterminateSecondaryColor
-    myDeterminatePrimaryColor = persistentConfigs.myDeterminatePrimaryColor
-    myDeterminateSecondaryColor = persistentConfigs.myDeterminateSecondaryColor
-    isAdvancedOptionsEnabled = persistentConfigs.isAdvancedOptionsEnabled
-    cycleTime = persistentConfigs.cycleTime
-    repaintInterval = persistentConfigs.repaintInterval
-  }
 }
